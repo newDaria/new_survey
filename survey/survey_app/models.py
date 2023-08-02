d07a7686796fa5620c169980ff8294e459cf1abe
Daria <d-step@mail.ru>
2023-08-02 08:53:27 +0700

    medels

--- survey/survey_app/models.py
+++ survey/survey_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 class Survey(models.Model):
@@ -24,11 +25,18 @@ class Option(models.Model):
     def __str__(self):
         return self.text
 
+class AnswerManager(models.Manager):
+    def user_submission_count_for_day(self, user,date):
+        today = timezone.now().date()
+        return self.filter(creator=user, created_at__date=today).count()
 
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE, default=None)
     option = models.ForeignKey(Option, on_delete=models.CASCADE)
     creator = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    objects = AnswerManager()
 
     def __str__(self):
         creator_username = self.creator.username if self.creator else "Unknown"
@@ -37,3 +45,5 @@ class Answer(models.Model):
         option_text = self.option.text if self.option else "Unknown"
 
         return f"{creator_username} - {survey_title} - {question_text} - {option_text}"
+
+
